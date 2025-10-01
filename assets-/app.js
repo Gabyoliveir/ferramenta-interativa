781f7766f4fedd437e35b46f87fe7dc3c065d4d2
Gabyoliveir <gabrielly.oliveira0405@escola.pr.gov.br>
2025-10-01 15:46:42 -0300

    g

--- assets-/app.js
+++ assets-/app.js
@@ -5,18 +5,12 @@ function criaCartao(categoria, pergunta, resposta) {
 }
 //17-09-25//
 cartao.innerHTML = `
-      
-                <div class="cartao__conteudo">
-            <h3>${categoria}<h3>
-
-                        <h3>Programação</h3>
-                        <div class="cartao__conteudo__pergunta">
-                                <p>${Qem é o homem que teve o poder de andar sobre o mar?}</p>
-                        </div>
-                        <div class="cartao__conteudo__resposta">
-                                <p>${O nome dEle é Jeus, seu nome é poder.}</p>
-                        </div>
-                </div>`
-      
-
-        container.appendChild(cartao)
+        <div class="cartao__conteudo">
+        <h3>${categoria}</h3>
+        <div class="cartao__conteudo__pergunta">
+                <p>${pergunta}</p>
+        </div>
+        <div class="cartao__conteudo__resposta">
+                <p>${resposta}</p>
+        </div>
+        </div>
